b4bd6dbb5ce9f1bc9892e1f3840b641370d39f23
audreyazura <audreyazura@fedora>
2021-03-06 03:35:40 +0900

    Capture probability as a function of overlap

--- src/afmluminescence/luminescencegenerator/QuantumDot.java
+++ src/afmluminescence/luminescencegenerator/QuantumDot.java
@@ -20,6 +20,8 @@ import com.github.audreyazura.commonutils.PhysicsTools;
 import com.github.kilianB.pcg.fast.PcgRSFast;
 import java.math.BigDecimal;
 import java.math.MathContext;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.nevec.rjm.BigDecimalMath;
 
 /**
@@ -54,6 +56,11 @@ public class QuantumDot extends AbsorberObject
      * The capture probability depends on many parameters and demand to be further investigate.
      * At the moment, it is approximated as the overlapping between the QD and the circle containing the positions the electron can reach
      * See here for the calculation of the overlap: https://www.xarg.org/2016/07/calculate-the-intersection-area-of-two-circles/
+     * It had to be slightly adapted with four different cases:
+     *  - when the electron is entirely inside the QD (electronDistance + electronSpan < radius)
+     *  - when the QD is entirely "inside" the electron (electronDistance + radius < electronSpan)
+     *  - when the center of the QD is farther away from the electron position than the intersection between the QD limit and the electron span limit (electronDistance > sqrt(abs(radius^2 - electronSpan^2)))
+     *  - when the center of the QD is closer from the electron position than the intersection between the QD limit and the electron span limit (electronDistance < sqrt(abs(radius^2 - electronSpan^2)))
      * @param p_RNG the random number generator
      * @param electronDistance the distance between the center of the QD and electron position
      * @param electronSpan the circle containing the position the electron can reach
@@ -61,13 +68,60 @@ public class QuantumDot extends AbsorberObject
      */
     synchronized public boolean capture(PcgRSFast p_RNG, BigDecimal electronDistance, BigDecimal electronSpan)
     {
-//        BigDecimal triangleBase = (m_radius.pow(2).add(electronDistance.pow(2)).subtract(electronSpan.pow(2))).divide(electronDistance.multiply(new BigDecimal("2")), MathContext.DECIMAL128);
-//        if (triangleBase.compareTo(BigDecimal.ZERO) < 0)
-//        {
-//            System.err.println("PROBLEM!!!!!!!!");
-//        }
+        double captureProba = 0;
         
-        double captureProba = 0.01;
+        //case if the electron is entirely inside the QD
+        if (electronDistance.add(electronSpan).compareTo(m_radius) <= 0)
+        {
+            captureProba = 1;
+        }
+        else
+        {
+            //if the QD is entirely in the electron span
+            if (electronDistance.add(m_radius).compareTo(electronSpan) <= 0)
+            {
+                captureProba = (m_radius.pow(2).divide(electronSpan.pow(2), MathContext.DECIMAL128)).doubleValue();
+            }
+            else
+            {
+                BigDecimal overlapArea = BigDecimal.ZERO;
+
+                //we compare the distance to sqrt(abs(radius^2 - electronSpan^2))
+                BigDecimal radiusDiff = BigDecimalMath.sqrt(((m_radius.pow(2)).subtract(electronSpan.pow(2))).abs(), MathContext.DECIMAL128);
+                
+                //if the QD center is farther away than the intersection points
+                if (electronDistance.compareTo(radiusDiff) >= 0)
+                {
+                    //the base of the triangle for the calculation here is (electronSpan^2 + electronDistance^2 - QDradius^2) / (2 * electronDistance)
+                    BigDecimal triangleBase = (electronSpan.pow(2).add(electronDistance.pow(2)).subtract(m_radius.pow(2))).divide(electronDistance.multiply(new BigDecimal("2")), MathContext.DECIMAL128);
+                    
+                    BigDecimal electronSlice = electronSpan.pow(2).multiply(BigDecimalMath.acos(triangleBase.divide(electronSpan, MathContext.DECIMAL128)));
+                    BigDecimal QDSlice = m_radius.pow(2).multiply(BigDecimalMath.acos((electronDistance.subtract(triangleBase)).divide(m_radius, MathContext.DECIMAL128)));
+                    BigDecimal triangleCorrection = electronDistance.multiply(BigDecimalMath.sqrt(electronSpan.pow(2).subtract(triangleBase.pow(2)), MathContext.DECIMAL128));
+                    
+                    overlapArea = electronSlice.add(QDSlice).subtract(triangleCorrection);
+                }
+                else
+                {
+                    //the base of the triangle for the calculation here is (electronSpan^2 - electronDistance^2 - QDradius^2) / (2 * electronDistance)
+                    BigDecimal triangleBase = (electronSpan.pow(2).subtract(electronDistance.pow(2)).subtract(m_radius.pow(2))).divide(electronDistance.multiply(new BigDecimal("2")), MathContext.DECIMAL128);
+                    
+                    BigDecimal electronSlice = electronSpan.pow(2).multiply(BigDecimalMath.acos((triangleBase.add(electronDistance)).divide(electronSpan, MathContext.DECIMAL128)));
+                    BigDecimal QDSlice = m_radius.pow(2).multiply(BigDecimalMath.pi(MathContext.DECIMAL128).subtract(BigDecimalMath.acos(triangleBase.divide(m_radius, MathContext.DECIMAL128))));
+                    BigDecimal triangleCorrection = electronDistance.multiply(BigDecimalMath.sqrt(m_radius.pow(2).subtract(triangleBase.pow(2)), MathContext.DECIMAL128));
+                    
+                    overlapArea = electronSlice.add(QDSlice).subtract(triangleCorrection);
+                }
+                
+                captureProba = overlapArea.divide(BigDecimalMath.pi(MathContext.DECIMAL128).multiply(electronSpan.pow(2)), MathContext.DECIMAL128).doubleValue();
+            }
+        }
+        
+        if (captureProba < 0 || captureProba > 1)
+        {
+            System.out.println("Probability has to be bound between 0 and 1");
+            Logger.getLogger(QuantumDot.class.getName()).log(Level.SEVERE, null, new ArithmeticException("Probability has to be bound between 0 and 1"));
+        }
         
         return p_RNG.nextDouble() < captureProba;
     }

3d35e38af24df585d9eca6d2b4b8f5fb8e6ddda0
audreyazura <audreyazura@fedora>
2021-03-10 20:07:06 +0900

    Better energy calculations

--- src/afmluminescence/luminescencegenerator/QuantumDot.java
+++ src/afmluminescence/luminescencegenerator/QuantumDot.java
@@ -34,22 +34,25 @@ public class QuantumDot extends AbsorberObject
     private final BigDecimal m_radius;
     private boolean m_recombined = false;
     
-    //ΔEg(InAs/GaAs) = 1.1 eV
+    //ΔEg(InAs/GaAs) ~ 1.1 eV
     public QuantumDot (BigDecimal p_positionX, BigDecimal p_positionY, BigDecimal p_radius, BigDecimal p_height)
     {
         BigDecimal two = new BigDecimal("2");
-        BigDecimal three = new BigDecimal("3");
-        BigDecimal pi = BigDecimalMath.pi(MathContext.DECIMAL128);
         PhysicsTools.Materials material = PhysicsTools.Materials.INAS;
         
         m_positionX = p_positionX;
         m_positionY = p_positionY;
         
         m_radius = p_radius;
+        BigDecimal equivalentSquareSide = m_radius.multiply(BigDecimalMath.sqrt(BigDecimalMath.pi(MathContext.DECIMAL128), MathContext.DECIMAL128));
+        
+        BigDecimal energyPlane = (energyParameter(equivalentSquareSide).divide(equivalentSquareSide, MathContext.DECIMAL128)).pow(2);
+        BigDecimal energyHeight = (energyParameter(p_height).divide(p_height, MathContext.DECIMAL128)).pow(2);
+        m_energy = material.getBaseBandgapSI().add((two.multiply(PhysicsTools.hbar.pow(2)).divide(material.getElectronEffectiveMassSI(), MathContext.DECIMAL128)).multiply((energyPlane.multiply(two)).add(energyHeight)));
         
         //at the moment, approximation of energy modeling the QD as a cube with side length L = (2*radius + height)/3
-        BigDecimal characteristicLength = ((p_radius.multiply(two)).add(p_height)).divide(three, MathContext.DECIMAL128);
-        m_energy = material.getBaseBandgapSI().add((three.multiply(PhysicsTools.hbar.pow(2)).multiply(pi.pow(2))).divide(two.multiply(material.getElectronEffectiveMassSI()).multiply(characteristicLength.pow(2)), MathContext.DECIMAL128));
+//        BigDecimal characteristicLength = ((p_radius.multiply(two)).add(p_height)).divide(three, MathContext.DECIMAL128);
+//        m_energy = material.getBaseBandgapSI().add((three.multiply(PhysicsTools.hbar.pow(2)).multiply(pi.pow(2))).divide(two.multiply(material.getElectronEffectiveMassSI()).multiply(characteristicLength.pow(2)), MathContext.DECIMAL128));
     }
     
     /**
@@ -124,6 +127,63 @@ public class QuantumDot extends AbsorberObject
         }
         
         return p_RNG.nextDouble() < captureProba;
+    }
+    
+    private BigDecimal energyParameter (BigDecimal size)
+    {
+        BigDecimal two = new BigDecimal(2);
+        BigDecimal halfpi = BigDecimalMath.pi(MathContext.DECIMAL128).divide(two, MathContext.DECIMAL128);
+        BigDecimal CBOffset = (new BigDecimal("0.7")).multiply(PhysicsTools.EV); //from https://aip.scitation.org/doi/abs/10.1063/1.125965, make it into PhysicalTools as a new enum, Metamaterials
+        BigDecimal u02 = PhysicsTools.Materials.INAS.getElectronEffectiveMassSI().multiply(size.pow(2)).multiply(CBOffset).divide(two.multiply(PhysicsTools.hbar.pow(2)), MathContext.DECIMAL128);
+        
+//        BigDecimal vi = new BigDecimal(Math.random()*Math.PI);
+//        BigDecimal error = new BigDecimal("1e-10");
+//        
+//        BigDecimal vprev = BigDecimal.ZERO;
+//        int counter = 0;
+//        do
+//        {
+//            System.out.println(counter);
+//            vprev = vi;
+//            
+//            BigDecimal fvi = (vi.pow(2).multiply(BigDecimal.ONE.add(BigDecimalMath.tan(vi).pow(2)))).subtract(u02);
+//            System.out.println(counter);
+//            BigDecimal fderivvi = two.multiply(vi).multiply(BigDecimal.ONE.add(vi.multiply(BigDecimalMath.tan(vi)).divide(BigDecimalMath.cos(vi).pow(2), MathContext.DECIMAL128)).add(BigDecimalMath.tan(vi).pow(2)));
+//            
+//            vi = (vprev.subtract(fvi.divide(fderivvi, MathContext.DECIMAL128))).abs();
+//            while (vi.compareTo(halfpi) >= 0)
+//            {
+//                //vi has to be between 0 and pi/2
+//                vi = vi.subtract(halfpi);
+//            }
+//            
+//            counter += 1;
+//            System.out.println(counter);
+//        }while(counter <= 10 && (vprev.subtract(vi)).abs().compareTo(error) >= 0);
+        
+        double vi = Math.random()*Math.PI;
+        double error = 1E-10;
+        
+        double vprev = 0;
+        int counter = 0;
+        do
+        {
+            vprev = vi;
+            
+            double fvi = Math.pow(vi, 2) * (1 + Math.pow(Math.tan(vi), 2)) - u02.doubleValue();
+            double fderivvi = 2 * vi * (1 + (vi * Math.tan(vi) / (Math.pow(Math.cos(vi), 2))) + Math.pow(Math.tan(vi), 2));
+            
+            vi = Math.abs(vprev - (fvi / fderivvi));
+            while (vi >= halfpi.doubleValue())
+            {
+                //vi has to be between 0 and pi/2
+                vi = vi - halfpi.doubleValue();
+            }
+            
+            counter += 1;
+        }while(counter <= 10 && Math.abs(vprev - vi) >= error);
+
+        return new BigDecimal(vi);
     }
     
     //will calculate probability based on phonon density

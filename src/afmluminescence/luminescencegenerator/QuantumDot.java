de97ca9143cc68284e163b2f962c751ca88da962
audreyazura <audreyazura@fedora>
2021-05-20 02:17:26 +0900

    Creation of the method to copy with a size change

--- src/afmluminescence/luminescencegenerator/QuantumDot.java
+++ src/afmluminescence/luminescencegenerator/QuantumDot.java
@@ -33,13 +33,14 @@ public class QuantumDot extends AbsorberObject
 {
     private final BigDecimal m_energy;
     private final BigDecimal m_radius;
+    private final BigDecimal m_height;
     private final double m_captureProba;
     private final double m_escapeProbability;
     private final double m_recombinationProbability;
     private boolean m_recombined = false;
     
     //ΔEg(InAs/GaAs) ~ 1.1 eV
-    public QuantumDot (BigDecimal p_positionX, BigDecimal p_positionY, BigDecimal p_radius, BigDecimal p_height, BigDecimal p_timeStep, ContinuousFunction p_captureTime, ContinuousFunction p_escapeTime)
+    public QuantumDot (BigDecimal p_positionX, BigDecimal p_positionY, BigDecimal p_radius, BigDecimal p_height, BigDecimal p_timeStep, ContinuousFunction p_captureTimes, ContinuousFunction p_escapeTimes)
     {
         BigDecimal two = new BigDecimal("2");
         BigDecimal eight = new BigDecimal("8");
@@ -50,7 +51,8 @@ public class QuantumDot extends AbsorberObject
         m_positionY = p_positionY;
         
         m_radius = p_radius.multiply(new BigDecimal("1"));
-        BigDecimal height = p_height.multiply(new BigDecimal("2")); //multiplied to have enough QDs that can capture (some problem with file?)
+        m_height = p_height;
+//        BigDecimal height = p_height.multiply(new BigDecimal("2")); //multiplied to have enough QDs that can capture (some problem with file?)
         BigDecimal equivalentSquareSide = m_radius.multiply(BigDecimalMath.sqrt(BigDecimalMath.pi(MathContext.DECIMAL128), MathContext.DECIMAL128));
         
         BigDecimal CBOffset = (new BigDecimal("0.7")).multiply(PhysicsTools.EV); //from https://aip.scitation.org/doi/abs/10.1063/1.125965, make it into PhysicalTools as a new enum, Metamaterials
@@ -81,11 +83,11 @@ public class QuantumDot extends AbsorberObject
         else
         {
             //else, the capture probability is calculated using P_capture = 1 - exp(-Δt/tau_capture), with tau_capture given in https://aip.scitation.org/doi/10.1063/1.1512694
-            m_captureProba = (BigDecimal.ONE.subtract(BigDecimalMath.exp(p_timeStep.negate().divide(p_captureTime.getValueAtPosition(m_radius), MathContext.DECIMAL128)))).doubleValue();
+            m_captureProba = (BigDecimal.ONE.subtract(BigDecimalMath.exp(p_timeStep.negate().divide(p_captureTimes.getValueAtPosition(m_radius), MathContext.DECIMAL128)))).doubleValue();
         }
         
         //the escape probability is calculated using P_capture = 1 - exp(-Δt/tau_escape) with tau_escape from https://aip.scitation.org/doi/10.1063/1.4824469
-        m_escapeProbability = (BigDecimal.ONE.subtract(BigDecimalMath.exp(p_timeStep.negate().divide(p_escapeTime.getValueAtPosition(m_radius), MathContext.DECIMAL128)))).doubleValue();
+        m_escapeProbability = (BigDecimal.ONE.subtract(BigDecimalMath.exp(p_timeStep.negate().divide(p_escapeTimes.getValueAtPosition(m_radius), MathContext.DECIMAL128)))).doubleValue();
         
         BigDecimal minPhotonEnergy = hostMaterial.getBaseBandgapSI().add(minPhononEnergy);
         
@@ -176,6 +178,23 @@ public class QuantumDot extends AbsorberObject
         return p_RNG.nextDouble() < reachingProbability * m_captureProba;
     }
     
+    public QuantumDot copyWithSizeChange(BigDecimal p_sizeMultiplier, BigDecimal p_timeStep, ContinuousFunction p_captureTimes, ContinuousFunction p_escapeTimes)
+    {
+        BigDecimal newRadius = m_radius;
+        BigDecimal newHeight = m_height;
+        
+        if (m_radius.compareTo(m_height) > 0)
+        {
+            newHeight = newHeight.multiply(p_sizeMultiplier);
+        }
+        else
+        {
+            newRadius = newRadius.multiply(p_sizeMultiplier);
+        }
+        
+        return new QuantumDot(m_positionX, m_positionY, newRadius, newHeight, p_timeStep, p_captureTimes, p_escapeTimes);
+    }
+    
     /**
      * See https://en.wikipedia.org/wiki/Finite_potential_well
      * Find v_0 using Newton's Method with the equation sqrt(u0^2 - v0^2) = v0*tan(v0)
@@ -222,7 +241,7 @@ public class QuantumDot extends AbsorberObject
     {
         return Math.pow(v, 2) * (1 + Math.pow(Math.tan(v), 2));
     }
-    
+
     //will calculate probability based on phonon density
     synchronized public boolean escape(PcgRSFast p_RNG)
     {
@@ -263,6 +282,6 @@ public class QuantumDot extends AbsorberObject
     @Override
     public String toString()
     {
-        return "(x = " + m_positionX + " ; y = " + m_positionY + " ; radius = " + m_radius;
+        return m_positionX + "\t" + m_positionY + "\t" + m_radius + "\t" + m_height + "\t" + m_energy;
     }
 }
